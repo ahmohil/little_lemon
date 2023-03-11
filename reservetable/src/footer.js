c323330339f1bc66e5725207b844585c409ffa7d
AbdulHananMohil <ahmohil78@gmail.com>
2023-03-11 09:59:41 +0500

    brought assets to src folder

--- reservetable/src/footer.js
+++ reservetable/src/footer.js
@@ -1,6 +1,19 @@
+import logo from "./icons_assets/Logo.svg"
+import Navbar
+ from "./components/navbar";
 function Footer()
 {
-
+    return(
+        <div className="footer">
+        <span className = "logo">
+           <img className = "logo" src={logo}/>
+        </span>
+        <nav className="verticalNavigation">
+            <h4>Doormat Navigation</h4>
+            <Navbar/>
+        </nav>
+        </div>
+    )
 }
 
 export default Footer;
--- reservetable/src/header.js
+++ reservetable/src/header.js
@@ -1,10 +1,16 @@
 import Icon from "./logo";
-
-
+import Navbar from "./components/navbar";
+import logo from "./icons_assets/Logo.svg"
 function Header(){
     return(
-        <Icon/>
-
+        <div className="header">
+        <span className = "logo">
+           <img className = "logo" src={logo}/>
+        </span>
+        <nav className="horizontalNavigation">
+            <Navbar/>
+        </nav>
+        </div>
     )
 }
 
